67d907ec4874fbfeede0a0b86937ba2aec0549b2
ranamohyy <ranaamr22@gmail.com>
2024-12-12 16:46:08 +0200

    make noData image png not networkimage

--- lib/core/assets.dart
+++ lib/core/assets.dart
@@ -6,14 +6,13 @@ class AppImage{
   static const String students ="assets/images/students.png";
   static const String design ="assets/images/design.png";
   static const String designN ="assets/images/designN.png";
-  static const String noData ="https://i.pinimg.com/1200x/e5/55/87/e55587d66eb7b637fc19ff959a6b04ab.jpg";
+  static const String noData ="assets/images/emptyView.png";
   static const String fawry ="https://d1b3667xvzs6rz.cloudfront.net/2018/09/4-2-Fawry.png";
   static const String chargeWallet ="assets/images/charge.svg";
   static const String results ="assets/images/results.svg";
   static const String user ="assets/images/user.svg";
   static const String courses ="assets/images/caourses.svg";
   static const String faq ="assets/images/graph.svg";
-
 
 
 }
