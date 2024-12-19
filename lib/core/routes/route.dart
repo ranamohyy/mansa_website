362428c51d4831fc6c01706820c6a389444235bc
ranamohyy <ranaamr22@gmail.com>
2024-12-19 23:58:26 +0200

    add animation in transction

--- lib/core/routes/route.dart
+++ lib/core/routes/route.dart
@@ -3,8 +3,24 @@ final GlobalKey<NavigatorState>navKey=GlobalKey<NavigatorState>();
 Route<dynamic>? onGenerateRouter(RouteSettings setting)=>null;
 class AppRouter{
   static final state=navKey.currentState!;
-  static Route<dynamic>_materialPageRoute(Widget page)=>MaterialPageRoute(builder: (context) =>page ,);
-  static BuildContext context=navKey.currentContext!;
+  static BuildContext context = navKey.currentContext!;
+  static Route<dynamic>_materialPageRoute(Widget page){
+    return PageRouteBuilder(pageBuilder: (context, animation, secondaryAnimation) =>page ,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(1.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.bounceIn;
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      var offsetAnimation = animation.drive(tween);
+return SlideTransition(
+  position: offsetAnimation,
+  child: child,
+);
+    },
+    );
+  }
+
+
   static Future<dynamic>navigateTo(Widget page){
     return state.push(
     _materialPageRoute(page));
