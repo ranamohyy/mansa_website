39172870b2c1a7db717cd571dc448e9fb1d79265
ranamohyy <ranaamr22@gmail.com>
2024-12-20 00:01:16 +0200

    no change

--- lib/devices/mobile/auth/widgets/logo_name_app.dart
+++ lib/devices/mobile/auth/widgets/logo_name_app.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:mansa/core/assets.dart';
 class LogoOfAppWidget extends StatelessWidget {
   const LogoOfAppWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return   Center(
-      child: AspectRatio(
-        aspectRatio: 0.4/0.2,
-        child:
-        Image.asset(
-            fit: BoxFit.fill,
-            AppImage.logo))
-    );
+      return Center(
+          child: AspectRatio(
+              aspectRatio: 0.4 / 0.2,
+              child:
+              Image.asset(
+                  fit: BoxFit.fill,
+                  AppImage.logo))
+      );
+    }
   }
-}
 
