7b740b9b0c33f5fd7c687840fce6caf7ea1aeffb
ranamohyy <ranaamr22@gmail.com>
2024-12-20 02:53:51 +0200

    finish myCourses view

--- lib/views/myProfile/body_my_courses.dart
+++ lib/views/myProfile/body_my_courses.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mansa/core/utils/constans.dart';
 import '../../core/res/responsive.dart';
-import '../../devices/mobile/myProfile/views/myCourses.dart';
 import '../home/CustomHomeForAllDevices.dart';
 import '../widgets/app_bar_helper.dart';
 class MyCourses extends StatelessWidget {
@@ -9,15 +8,21 @@ class MyCourses extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (Responsive.isMob(context)){return  const  MobileMyCourses(
-      type: ourStudent,
-    );}
-    else {
-      return Scaffold(
-          backgroundColor: Colors.grey[100],
-          appBar: Responsive.isTab(context)?const AppBarHelper(text: "courses"):null,
+    print(scaffoldBackGroundColor.runtimeType);
+    //لما اخد العدد من api هستخدم دا
+    //CustomScrollView(slivers: [
+    //           CustomItemCoursesForHomeAndCourses(
+    //             interScreen: "myCourses",
+    //             index: 3,
+    //             itemsCount: 2,
+    //             type: type,)])
+      return
+          Scaffold(
+              backgroundColor:scaffoldBackGroundColor,
+          appBar: Responsive.isTab(context)||Responsive.isMob(context)?
+          const AppBarHelper(text: "courses"):null,
           body:
          const CustomHomeForAllDevices(type: ourStudent,)
       );
-    }  }
+     }
 }
