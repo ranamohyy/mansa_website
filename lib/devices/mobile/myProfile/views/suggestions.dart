3d3dd3473dd3022bf10555b13f045768d2a05c0f
ranamohyy <ranaamr22@gmail.com>
2024-12-20 03:52:51 +0200

    add fawry logo as png

--- lib/devices/mobile/myProfile/views/suggestions.dart
+++ lib/devices/mobile/myProfile/views/suggestions.dart
@@ -5,37 +5,45 @@ import 'package:mansa/core/helpers/custom_my_input.dart';
 import 'package:mansa/core/utils/constans.dart';
 import 'package:mansa/devices/mobile/auth/widgets/text_auth.dart';
 import 'package:mansa/views/widgets/app_bar_helper.dart';
+
+import '../../../../core/res/responsive.dart';
+import '../../../../views/widgets/custom_button_for_devices.dart';
 class Suggestions extends StatelessWidget {
   const Suggestions({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: scaffoldBackGroundColor,
       appBar:const  AppBarHelper(text: "FAQ"),
       body: SingleChildScrollView(
         child: Padding(
           padding:  EdgeInsets.symmetric(horizontal: 8.w,vertical: 30.h),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
               TextAuth(text: "enter suggestion"),
-              SizedBox(height: 4.h,),
-              MyInput(
-                  text: "Type your suggestion"),
+              const SizedBox(height: 18,),
+              SizedBox(
+                width:Responsive.isWeb(context)?100.w :280,
+                child: MyInput(
+                    text: "Type your suggestion"),
+              ),
               SizedBox(height:20.h),
               Center(
                 child: SizedBox(
                   width: 100.w,
-                  child: MyButton(
-                      onPressed: (){},
-                      child:TextAuth(text: "send",style:  kTextStyle20white.copyWith(fontSize: 17.sp),) ),
-                ),
+                  child:CustomButtonForDevices(onPressed: (){},
+                style:kTextStyle20white,
+                text:"إرسال",
+                width: 170,color: kPrimaryColor,
               )
 
+        )
+
+              )
             ],
           ),
         ),
