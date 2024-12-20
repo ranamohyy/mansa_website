07e972560efc7a1fab915fd91742bf1dfce38e22
ranamohyy <ranaamr22@gmail.com>
2024-12-20 02:56:04 +0200

    put scaffold

--- lib/devices/mobile/myProfile/views/results.dart
+++ lib/devices/mobile/myProfile/views/results.dart
@@ -18,36 +18,41 @@ final List <String> label = [
   @override
   Widget build(BuildContext context) {
     return
-      Responsive.isWeb(context)?Expanded(child: Padding(
-        padding:  EdgeInsets.only(left: MediaQuery.of(context).size.width/8),
-        child: Center(
-          child: Column(children:
-          List.generate(label.length,
-                (index) => Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Card(
-                shape: Border.symmetric(horizontal:BorderSide(
-          style: BorderStyle.solid,
-                    width: 0.w)),
-                color:Colors.white,
-                elevation: 0.5,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    TextAuth(text:label[index ] ) ,
-                    TextAuth(text: results[index],style: kTextStyle15.copyWith(color: kSecondColor),)
-                  ],
+      Responsive.isWeb(context)?
+      Scaffold(
+        backgroundColor: scaffoldBackGroundColor,
+        body: Expanded(child: Padding(
+          padding:  EdgeInsets.only(left: MediaQuery.of(context).size.width/8),
+          child: Center(
+            child: Column(
+                children:
+            List.generate(label.length,
+                  (index) => Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Card(
+                  shape: Border.symmetric(horizontal:BorderSide(
+            style: BorderStyle.solid,
+                      width: 0.w)),
+                  color:Colors.white,
+                  elevation: 0.5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      TextAuth(text:label[index ] ) ,
+                      TextAuth(text: results[index],style: kTextStyle15.copyWith(color: kSecondColor),)
+                    ],
+                  ),
                 ),
               ),
-            ),
-          )
+            )
 
+            ),
           ),
         ),
-      ),
+        ),
       ):
       Scaffold(
-      backgroundColor:Colors.white,
+      backgroundColor:scaffoldBackGroundColor,
       appBar:const  AppBarHelper(text: "results"),
       body:
       Column(children:
