99043d1f97af1556a9d919b025b6a20725a67e62
ranamohyy <ranaamr22@gmail.com>
2024-12-20 02:56:27 +0200

    delete some files

--- lib/devices/chrome/myProfile.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class WebMyProfile extends StatelessWidget {
-  const WebMyProfile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
-  }
-}
