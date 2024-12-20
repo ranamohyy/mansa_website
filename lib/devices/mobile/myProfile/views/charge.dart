83a30b54d06ffa0cca38475593e39e6ee63d22d7
ranamohyy <ranaamr22@gmail.com>
2024-12-20 03:54:32 +0200

    add fawry logo as png

--- lib/devices/mobile/myProfile/views/charge.dart
+++ lib/devices/mobile/myProfile/views/charge.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mansa/core/assets.dart';
 import 'package:mansa/core/helpers/custom_button.dart';
+import 'package:mansa/core/helpers/custom_my_input.dart';
+import 'package:mansa/core/res/responsive.dart';
 import 'package:mansa/core/routes/route.dart';
 import 'package:mansa/core/utils/constans.dart';
 import 'package:mansa/views/home/home.dart';
 import 'package:mansa/views/widgets/app_bar_helper.dart';
+import 'package:mansa/views/widgets/custom_button_for_devices.dart';
 class ChargeWallet extends StatelessWidget {
   const ChargeWallet({super.key});
 
@@ -14,22 +16,31 @@ class ChargeWallet extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: Colors.white,
-      appBar: const AppBarHelper(text: "المحفظة"),
+      appBar: Responsive.isWeb(context)?null:const AppBarHelper(text: "المحفظة"),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Image.network(AppImage.fawry,),
+            AspectRatio(
+                aspectRatio: 0.04/0.007,
+                child: Image.asset(AppImage.fawry,)),
             SizedBox(height: 12.h,),
 
             const Text("برجاء التوجهه إلي أقرب ماكينة فوري لشحن المحفظة سيتم إرسال كود علي هاتفك وثم شراء الكورس"),
-            SizedBox(height: 12.h,)
+            SizedBox(height: 12.h,),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 135.w,vertical: 10),
+                child: MyInput(text: "أدخل الكود هنا",))
             ,SizedBox(
                 width: 110.w,
-                child: MyButton(onPressed: (){
-                  AppRouter.navigateTo(HomePage(type: ourStudent));
-
-                }, child: Text("شحن",style: kTextStyle20white.copyWith(fontSize: 17.sp),)))
+                child: CustomButtonForDevices(onPressed: (){
+                  AppRouter.navigateTo(const HomePage(type: ourStudent));
+                },
+                  style:kTextStyle20white,
+                  text:"شحن",
+                   width: 150,color: kPrimaryColor,
+                )
+            )// Text(,style: kTextStyle20white.copyWith(fontSize: 17.sp),)))
           ],
         ),
       ),
