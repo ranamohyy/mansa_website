57e413b81cd501014bdc9661c3cd4111668d7d8a
ranamohyy <ranaamr22@gmail.com>
2024-12-21 22:45:57 +0200

    make the text at the start of the screen

--- lib/views/myProfile/views/aboutApp.dart
+++ lib/views/myProfile/views/aboutApp.dart
@@ -14,17 +14,23 @@ class Aboutapp extends StatelessWidget {
       appBar:
       Responsive.isWeb(context)?null:
       const AppBarHelper(text: "aboutApp"),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            TextAuth(
-                style: kTextStyle11grey.copyWith(fontSize: 17),
-                text: "هذا التطبيق هنا لمساعدتك لمزاكرة و فهم ماده الكيمياء مع الأستاد احمد الدجلة و متابعة مستواك مع وجود امتحانات للمتابعة."
-                )
-          ],
+      body: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: TextAuth(
+                    style: kTextStyle11grey.copyWith(fontSize: 17),
+                    text: "هذا التطبيق هنا لمساعدتك لمزاكرة و فهم ماده الكيمياء مع الأستاد احمد الدجلة و متابعة مستواك مع وجود امتحانات للمتابعة."
+                    ),
+              )
+            ],
+          ),
         ),
       ),
     );
