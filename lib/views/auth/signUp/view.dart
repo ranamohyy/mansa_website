ee3429d7df2cb7f99f2837709eb1d46612abe702
ranamohyy <ranaamr22@gmail.com>
2024-12-21 22:51:52 +0200

    convert to stateless

--- lib/views/auth/signUp/view.dart
+++ lib/views/auth/signUp/view.dart
@@ -3,12 +3,8 @@ import 'package:mansa/core/res/responsive.dart';
 import 'package:mansa/devices/tablet/signUp.dart';
 import '../../../devices/chrome/signUp.dart';
 import '../../../devices/mobile/signUp.dart';
-class SignupView extends StatefulWidget {
+class SignupView extends StatelessWidget {
   const SignupView({super.key});
-  @override
-  State<SignupView> createState() => _SignupViewState();
-}
-class _SignupViewState extends State<SignupView> {
   @override
   Widget build(BuildContext context) {
     print(MediaQuery.of(context).size.width);
